097a92158565410c201df71e179ae8e0936a72ee
moisesISAL <moises@isaldigital.com>
2021-05-15 22:00:56 -0500

    minor lighthouse improvements

--- components/LanguageSelector.jsx
+++ components/LanguageSelector.jsx
@@ -6,8 +6,8 @@ const LanguageSelector = () => {
     const router = useRouter()
     return (
         <div className='lang-div'>
-            <Link  href={`/en`} locale={false} ><a className={`lang ${router.locale == 'en' ? 'gm-disabled':'gm-sec'}`}><img src='https://res.cloudinary.com/dcemx7jg0/image/upload/v1620663404/isaldigital/united-states_gghs5h.svg'></img><p>EN</p></a></Link>
-            <Link href={`/es`} locale={false}><a className={`lang ${router.locale == 'es' ? 'gm-disabled':'gm-sec'}`}><img src='https://res.cloudinary.com/dcemx7jg0/image/upload/v1620663404/isaldigital/mexico_a34y9i.svg'></img><p>ES</p></a></Link>
+            <Link alt='lang: EN' width="20" height="20" href={`/en`} locale={false} ><a className={`lang ${router.locale == 'en' ? 'gm-disabled':'gm-sec'}`}><img src='https://res.cloudinary.com/dcemx7jg0/image/upload/v1620663404/isaldigital/united-states_gghs5h.svg'></img><p>EN</p></a></Link>
+            <Link alt='lang: ES' width="20" height="20" href={`/es`} locale={false}><a className={`lang ${router.locale == 'es' ? 'gm-disabled':'gm-sec'}`}><img src='https://res.cloudinary.com/dcemx7jg0/image/upload/v1620663404/isaldigital/mexico_a34y9i.svg'></img><p>ES</p></a></Link>
         </div>
     )
 }
