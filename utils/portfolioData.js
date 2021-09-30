01f53f270b9d13478d01becdff4ec575a5e994f5
moyp99 <moypg99@gmail.com>
2021-09-30 14:03:07 -0500

    portfolio update

--- utils/portfolioData.js
+++ utils/portfolioData.js
@@ -7,33 +7,36 @@ export const portfolioData =
       {
         title: "Moises.tk",
         description: "Este es el portafolio personal de nuestro founder. El objetivo de un portafolio es resaltar la capacidad profesional de los individuos. ",
-        technologies:
-          "ReactJS, SCSS, Firebase",
+        technologies:"ReactJS, SCSS, Firebase",
         img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/moises_plqn3c.jpg',
         link: "https://www.moises.tk",
       },
       {
         title: "Colmena.fund",
         description: "Landing page para una startup. Logramos llevarla hasta el campamento nacional posible y a la final del premio Santander a la innovación empresarial.",
-        technologies:
-          "HTML, CSS, Bootstrap, Firebase, SASS",
+        technologies:"HTML, CSS, Bootstrap, Firebase, SASS",
         img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729742/isaldigital/portfolio/web-Colmena_bftf0n.jpg',
         link: "https://www.colmena.fund",
       },
+      // {
+      //   title: "Travel Website",
+      //   description: "Página web de prueba. El objetivo de este desarrollo fue implementar nuevas animaciones y transiciones para crear experiencias únicas.",
+      //   technologies:
+      //     "Javascript, HTML, CSS",
+      //   img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-travel_htcvgx.jpg',
+      //   link: "https://isaldigital.com/mjpg/travel-website",
+      // },
       {
-        title: "Travel Website",
-        description: "Página web de prueba. El objetivo de este desarrollo fue implementar nuevas animaciones y transiciones para crear experiencias únicas.",
-        technologies:
-          "Javascript, HTML, CSS",
-        img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-travel_htcvgx.jpg',
-        link: "https://isaldigital.com/mjpg/travel-website",
+        title: "Auremp",
+        technologies: "React JS, NextJS, Magic, Strapi, Nodejs",
+        img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/c_scale,w_432/v1633028340/isaldigital/portfolio/auremp_m5mz2s.png',
+        link: "https://auremp.com",
+        date: "June 2021"
       },
-      
       {
         title: "I-Design (Offline)",
         description: "Página web corporativa. Esta página fue una de las primeras en la que trabajamos y fue desarrollada desde 0.  Desafortunadamente se encuentra offline",
-        technologies:
-          "HTML, CSS, Bootstrap, PHP, SASS",
+        technologies: "HTML, CSS, Bootstrap, PHP, SASS",
         img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729741/isaldigital/portfolio/web-IDesign_znfjcq.jpg',
         link: "",
       },
@@ -41,7 +44,7 @@ export const portfolioData =
         title: "Luxury (Offline)",
         description: "Página web promocional. Diseñada con el objetivo de conseguir más clientes y hacer reservas en línea. La página aun no esta terminada.",
         technologies:
-          "HTML, CSS, Bootstrap, SASS (En progreso)",
+          "HTML, CSS, Bootstrap, SASS",
         img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729742/isaldigital/portfolio/web-luxury_frzexv.jpg',
         link: "",
       },
@@ -75,22 +78,22 @@ export const portfolioData =
         img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/v1614729740/isaldigital/portfolio/angular-budget_cvrjlp.jpg',
         link: "https://budget-mjpg.web.app/",
       },
-      {
-        title: "Cool TODO List",
-        description: "Aplicación de agenda. El objetivo de esta aplicación fue probar las capacidades de javascript por si solo.",
-        technologies:
-          "Javascript, HTML, CSS",
-        img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-todo_wfwfpj.jpg',
-        link: "https://isaldigital.com/mjpg/todo-project",
-      },
-      {
-        title: "Coloors Pallete",
-        description: "Aplicación para generar paletas de colores. El objetivo de esta aplicación fue probar las capacidades de javascript por si solo.",
-        technologies:
-          "Javascript, HTML, CSS",
-        img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-coloors_bnkrxu.jpg',
-        link: "https://isaldigital.com/mjpg/coloors",
-      },
+      // {
+      //   title: "Cool TODO List",
+      //   description: "Aplicación de agenda. El objetivo de esta aplicación fue probar las capacidades de javascript por si solo.",
+      //   technologies:
+      //     "Javascript, HTML, CSS",
+      //   img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-todo_wfwfpj.jpg',
+      //   link: "https://isaldigital.com/mjpg/todo-project",
+      // },
+      // {
+      //   title: "Coloors Pallete",
+      //   description: "Aplicación para generar paletas de colores. El objetivo de esta aplicación fue probar las capacidades de javascript por si solo.",
+      //   technologies:
+      //     "Javascript, HTML, CSS",
+      //   img: 'https://res.cloudinary.com/dcemx7jg0/image/upload/f_auto,q_auto:eco/v1614729740/isaldigital/portfolio/js-coloors_bnkrxu.jpg',
+      //   link: "https://isaldigital.com/mjpg/coloors",
+      // },
       
       
     ],
